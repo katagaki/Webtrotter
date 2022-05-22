554bea6bfb06790a89b2cf66a0ffd2075fe8170c
katagaki <68231035+katagaki@users.noreply.github.com>
2022-05-22 19:22:33 +0800

    Initial Commit

---
+++ Webtrotter/WebtrotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebtrotterApp.swift
+//  Webtrotter
+//
+//  Created by 堅書 on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WebtrotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
